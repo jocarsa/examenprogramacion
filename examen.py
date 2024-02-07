a7c89d4a61d8d1ff55933565be8e37e9eb52d5fd
= <info@josevicentecarratala.com>
2024-02-07 18:23:43 +0100

    creamos el menu

---
+++ examen.py
@@ -0,0 +1,11 @@
+def menu():
+    print("Elige una opción:")
+    print("1.-Listado de registros")
+    print("2.-Buscar registros")
+    print("3.-Insertar registros")
+    print("4.-Actualizar registros")
+    print("5.-Eliminar registros")
+    print("6.-Salir")
+    menu()
+
+menu()
